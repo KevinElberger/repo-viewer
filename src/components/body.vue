ae6c2d92dc9f4358be51260732f7658fc691ac3e
Kevin <kevin@Kevins-MBP.attlocal.net>
2017-11-27 20:38:11 -0800

    Remove hello world component, add body, footer components

---
+++ src/components/body.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="body">
+    <h1>Repo Viewer</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'footerView',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #fff;
+  font-weight: normal;
+}
+</style>
