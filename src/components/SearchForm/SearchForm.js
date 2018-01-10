e02d9a4114f0b80488f64c7424d4dbc39ec94f74
Kevin Elberger <kevelberger@gmail.com>
2018-01-09 16:05:32 -0800

    Remove api folder, logo. Add SearchForm and Loader component

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.props.onSubmit && typeof this.props.onSubmit === 'function') {
+      this.props.onSubmit(this.state.value);
+    }
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <form action="" onSubmit={this.handleSubmit}>
+          <input 
+            type="text" 
+            placeholder="Search for a repository"
+            value={this.state.value}
+            onChange={this.handleChange} 
+          />
+          <button type="submit">
+            <i className="fa fa-search"></i>
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchForm;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,31 +1,40 @@
 import React, { Component } from 'react';
 import './home.css';
+import Loader from '../../components/Loader/Loader';
 import Footer from '../../components/Footer/Footer';
+import SearchForm from '../../components/SearchForm/SearchForm';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      repos: [],
       loading: false
-    }
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
+  handleSubmit(value) {
+    this.setState({ loading: true });
+    const apiRootUrl = 'https://api.github.com/';
+    fetch(`${apiRootUrl}${value}`)
+      .then(response => {
+        this.setState({ loading: false, repos: response.json() });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   render() {
+    const loading = this.state.loading;
+
+    /* {loading ? <Loader /> : <SearchForm onSubmit={this.handleSubmit} />} */
+
     return (
       <div className="home">
         <h1 className="title">Repo Viewer</h1>
-        <div className="search">
-          <form action="" onSubmit={this.handleSubmit}>
-            <input type="text" placeholder="Search for a repository" />
-            <button type="submit">
-              <i className="fa fa-search"></i>
-            </button>
-          </form>
-        </div>
+        <Loader />
         <Footer />
       </div>
     );
