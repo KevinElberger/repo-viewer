e800b1af2b088e18da21608329a5b994d85c8c98
Kevin Elberger <kevelberger@gmail.com>
2018-01-12 08:47:16 -0800

    Fix detail panel CSS alignment issue

--- src/components/StatPanel/StatPanel.js
+++ src/components/StatPanel/StatPanel.js
@@ -24,32 +24,34 @@ class StatPanel extends Component {
 
     return (
       <div className={this.props.data.panelName} onClick={this.handleClick}>
-        <p className="result-text">
-          {this.props.data.resultText}
-        </p>
-        <div className="wrap">
-          <figure className="chart" data-percent="100">
-            <svg width="120" height="120">
-              <circle 
-                  className="outer"
-                  cx="130" 
-                  cy="60" 
-                  r="40" 
-                  transform="rotate(-90, 95, 95)"
+        <div className="panel-wrap">
+          <p className="result-text">
+            {this.props.data.resultText}
+          </p>
+          <div className="wrap">
+            <figure className="chart" data-percent="100">
+              <svg width="120" height="120">
+                <circle 
+                    className="outer"
+                    cx="130" 
+                    cy="60" 
+                    r="40" 
+                    transform="rotate(-90, 95, 95)"
+                />
+              </svg>
+            </figure>
+            <p className="stat">
+              <CountUp
+                start={0}
+                end={Number(this.props.data.panelNumber)}
+                duration={1.5}
               />
-            </svg>
-          </figure>
-          <p className="stat">
-            <CountUp
-              start={0}
-              end={Number(this.props.data.panelNumber)}
-              duration={1.5}
-            />
+            </p>
+          </div>
+          <p className="result-footer-text">
+            {this.props.data.footerText}
           </p>
         </div>
-        <p className="result-footer-text">
-          {this.props.data.footerText}
-        </p>
         <div className={detailClassName}>
           <p className="detail-title">
             {this.props.data.detailTitle}
