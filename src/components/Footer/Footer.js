c1f117f0efe9513cc0f8ea00eab0d10d9a071af7
Kevin Elberger <kevelberger@gmail.com>
2018-02-01 10:41:55 -0800

    Change Footer, Loader components to be functional

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
 import './footer.css';
 
-class Footer extends Component {
-    render() {
-        return (
-            <div className="footer">
-                <p className="small">
-                    Developed By Kevin Elberger 
-                </p>
-                <a 
-                    className="github"
-                    href="https://www.github.com/KevinElberger/repo-viewer"
-                >
-                    <i className="fa fa-github"></i>
-                </a>
-            </div>
-        );
-    }
-}
+const Footer = () => (
+    <div className="footer">
+        <p className="small">
+            Developed By Kevin Elberger 
+        </p>
+        <a 
+            className="github"
+            href="https://www.github.com/KevinElberger/repo-viewer"
+        >
+            <i className="fa fa-github"></i>
+        </a>
+    </div>
+);
 
 export default Footer;
--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import './loader.css';
 
-class Loader extends Component {
-  render() {
-    return (
-      <div className="loaderContainer">
-        <div className="loader">
-          <svg className="circular" viewBox="25 25 50 50">
-            <circle 
-              className="path" cx="50" cy="50" r="20" fill="none" 
-              strokeWidth="2" strokeMiterlimit="10"
-            />
-          </svg>
-        </div>
-      </div>
-    );
-  }
-}
+const Loader = () => (
+  <div className="loaderContainer">
+    <div className="loader">
+      <svg className="circular" viewBox="25 25 50 50">
+        <circle 
+          className="path" cx="50" cy="50" r="20" fill="none" 
+          strokeWidth="2" strokeMiterlimit="10"
+        />
+      </svg>
+    </div>
+  </div>
+);
 
 export default Loader;
