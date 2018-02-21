67f568e50e6c166dc727f3db309efb320d0e0716
Kevin Elberger <kevelberger@gmail.com>
2018-02-21 11:39:23 -0800

    Add Enzyme for unit tests; Add App unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
+});
+
+it('should have the correct state', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.state().resize).toBe(false);
+});
+
+it('should have the correct number of divs', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('div').length).toBe(2);
+});
+
+it('should have the div with the container classname', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.container').length).toBe(1);
+});
+
+it('should have the container-expand class if resize is true', () => {
+  const wrapper = shallow(<App />);
+  wrapper.setState({ resize: true });
+  expect(wrapper.find('.expand').length).toBe(1);
 });
